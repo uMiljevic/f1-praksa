83bd45e0119c85787750c8617e8dd9871d54a743
vexy_85 <vstevanov@gmail.com>
2024-05-28 14:25:53 +0200

    Add new URL

--- src/Components/DriverDetails.js
+++ src/Components/DriverDetails.js
@@ -1,48 +1,63 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useParams } from "react-router-dom";
 
 export default function Drivers() {
     const [driverDetails, setDriverDetails] = useState([]);
+    const [driverResults, setDtiverResults] = useState([]);
+    const params = useParams();
 
     useEffect(() => {
         getDriverDetails();
     }, []);
 
+
     const getDriverDetails = async () => {
-        const url = "http://ergast.com/api/f1/2013/driverStandings.json";
-        const response = await axios.get(url);
+        const driverId = params.driverId;
+        const urlStandings = `http://ergast.com/api/f1/2013/drivers/${driverId}/driverStandings.json`;
+        const urlResults = `http://ergast.com/api/f1/2013/${driverId}/results.json`;
+
+        const response = await axios.get(urlStandings);
+        const responeseResults = await axios.get(urlResults);
+
         // console.log(response.data.MRData.StandingsTable.StandingsLists[0]);
         setDriverDetails(response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
-    };
+        setDriverResults(responeseResults.data.MRData.StandingsTable);
 
+    }
 
+    // console.log(responeseResults);
     return (
         <div>
-            <div>
-                <tr key={detail.Driver.driverId}>
-                    <td>{detail.Driver.nationality} </td>
-                    <td>{detail.Constructors[0].name} </td>
-                    <td>{detail.Driver.dateOfBirth} </td>
-                    <td>Biography:  </td>
-                </tr>
-            </div>
+            {driverDetails.map((detail) => {
+                return (
+                    <ul key={detail.Driver.driverId}>
+                        <li>Country: {detail.Driver.nationality} </li>
+                        <li>Team: {detail.Constructors[0].name} </li>
+                        <li>Birth: {detail.Driver.dateOfBirth} </li>
+                        <li>Biography: </li>
+                    </ul>
+
+                );
+            })}
+
             <table className="table">
                 <tbody>
-                    {driverDetails.map((detail) => {
-                        console.log(detail);
+                    {driverResults.map((results) => {
                         return (
                             <div>
-                                <tr key={detail.Driver.driverId}>
-                                    <td>{detail.Driver.nationality} </td>
-                                    <td>{detail.Constructors[0].name} </td>
-                                    <td>{detail.Driver.dateOfBirth} </td>
-                                    <td>Biography:  </td>
+                                <tr key={results.driverId}>
+                                    <td>{results.round} </td>
+                                    <td>{results.Constructors[0].name} </td>
+                                    <td>{results.position} </td>
+                                    <td>  </td>
                                 </tr>
                             </div>
                         );
                     })}
                 </tbody>
+
             </table>
         </div>
-    );
-}
+    )
+};
