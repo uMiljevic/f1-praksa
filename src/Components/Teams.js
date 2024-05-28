191c63bec15e8d8c69184937d74a955a4eb195b0
JelenaNikolicGit <jelenanikol9@gmail.com>
2024-05-28 15:50:12 +0200

    Add Teams

--- src/Components/Teams.js
+++ src/Components/Teams.js
@@ -1,35 +1,35 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function App() {
   const [teams, setTeams] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     getTeams();
-
   }, []);
-
 
   const getTeams = async () => {
     const urlAllteams = "http://ergast.com/api/f1/2013/constructorStandings.json";
     const response = await axios.get(urlAllteams);
     console.log(response.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings);
     setTeams(response.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings);
-
   }
 
-  //console.log(teams);
-
+  const handleGetTeamDetails = (constructorId) => {
+    const linkTo = `/teamDetails/${constructorId}`;
+    navigate (linkTo);
+  };
 
   return (
     <div className="teams">
       <table className="table">
         <thead>
-            <th>Position</th>
-            <th>Name</th>
-            <th>Details</th>
-            <th>Points</th>
+          <th>Position</th>
+          <th>Name</th>
+          <th>Details</th>
+          <th>Points</th>
         </thead>
         <tbody>
           {teams.map((team, i) => {
@@ -37,14 +37,14 @@ export default function App() {
             return (
               <tr key={i}>
                 <td>{team.position}</td>
-                <td>{team.Constructor.name}</td>
+                <td onClick={() => handleGetTeamDetails(team.Constructor.constructorId)}>{team.Constructor.name}</td>
                 <td><a href={team.Constructor.url} target="_blank">Details</a></td>
                 <td>{team.points}</td>
               </tr>
             );
           })};
-</tbody>
-</table>
+        </tbody>
+      </table>
 
 
     </div>
