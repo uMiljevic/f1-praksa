0f84e0843cff78434656edda435927057d4d3b58
Duler76 <dusanradovic76@gmail.com>
2024-05-28 09:45:40 +0200

    Add Races again

--- src/Components/Races.js
+++ src/Components/Races.js
@@ -1,7 +1,63 @@
-export default function Races(){
-    return(
+
+
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+
+
+
+export default function Races() {
+
+    const [allRaces, setAllRaces] = useState([]);
+    // const [raceQualifiers, setRaceQualifiers] = useState([]);
+    // const [raceResults, setRaceResults] = useState([]);
+
+    useEffect(() => {
+        getAllRaces();
+    }, []);
+
+    const getAllRaces = async () => {
+        const urlAllRaces = "https://ergast.com/api/f1/2013/results/1.json";
+        const response = await axios.get(urlAllRaces);
+        console.log(response.data.MRData.RaceTable.Races);
+        setAllRaces(response.data.MRData.RaceTable.Races);
+    }
+
+    return (
         <div>
-            <h1>Races</h1>
+            <h1>Race calendar 2013</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Round</th>
+                        <th>Grand Prix</th>
+                        <th>Circuit</th>
+                        <th>Date</th>
+                        <th>Winner</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {allRaces.map((race) => {
+                        console.log(race);
+                        return (
+                            <tr >
+                                <td>{race.round}</td>
+                                <td>{race.raceName}</td>
+                                <td>{race.Circuit.circuitName}</td>
+                                <td>{race.date}</td>
+                                <td>{race.Results[0].Driver.familyName}</td>
+                            </tr>
+                            
+                        )
+
+                    })}
+                </tbody>
+            </table>
+
         </div>
     );
+
+
+
 }
