1c44055e7e9561f0f6d9594277662d7ef6694396
uMiljevic <uros.milje@gmail.com>
2024-05-29 15:37:29 +0200

    Added All Flags In Utils

--- src/Utils.js
+++ src/Utils.js
@@ -1,8 +1,47 @@
+
+const getAllCountries = [
+    {
+        alpha_2_code: "GB",
+        en_short_name: "UK",
+        nationality: "British"
+    },
+    {
+        alpha_2_code: "NL",
+        en_short_name: "Dutch",
+        nationality: "Netherlandic"
+    },
+    {
+        alpha_2_code: "AE",
+        en_short_name: "UAE",
+        nationality: "Emirian"
+    },
+    {
+        alpha_2_code: "KR",
+        en_short_name: "Korea",
+        nationality: "Korean"
+    },
+    {
+        alpha_2_code: "US",
+        en_short_name: "USA",
+        nationality: "United States"
+    }
+]
+
 export function getAlphaCode(flags, value) {
+    
     const flagsData = flags.filter(flag =>
-        flag.en_short_name.toLowerCase() === value.toLowerCase() ||
+        flag.en_short_name.toLowerCase() === value.toLowerCase()
+        ||
         flag.nationality.toLowerCase() === value.toLowerCase()
     );
-    return flagsData.length === 1 ? flagsData[0].alpha_2_code : value;
 
+    if(flagsData.length === 1) return flagsData[0].alpha_2_code;
+    
+    const allCountriesFlags = getAllCountries.filter(flag =>
+        flag.en_short_name.toLowerCase() === value.toLowerCase()
+        ||
+        flag.nationality.toLowerCase() === value.toLowerCase()
+    );
+
+    return allCountriesFlags.length === 1 ? allCountriesFlags[0].alpha_2_code : value;
 }
