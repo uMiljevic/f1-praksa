25bfdc9f6d1a49aa4e9f1f2b16f2b038f458ef94
vexy_85 <vstevanov@gmail.com>
2024-05-29 12:39:53 +0200

    add commit

--- src/Components/Drivers.js
+++ src/Components/Drivers.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Flag from 'react-flagkit';
+
 
 
 export default function Drivers() {
@@ -15,10 +17,20 @@ export default function Drivers() {
         const url = "http://ergast.com/api/f1/2013/driverStandings.json";
         const response = await axios.get(url);
         console.log(response.data.MRData.StandingsTable.StandingsLists[0]);
-        setDrivers(response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);  
+        setDrivers(response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
     }
 
-    const urlFlag = "https://raw.githubusercontent.com/Dinuks/country-nationality-list/master/countries.json";
+    // const getFlags = (filter, size) => {
+    //     const flagData = props.flags.filter(flags =>
+    //         flags.en_short_name.toLowerCase() === filter.toLowerCase() ||
+    //         flags.nationality.toLowerCase() === filter.toLowerCase()
+    //     );
+
+    //     // const alpha2Code = flagData.lenght == 1 ? flagData[0].alpha_2_code : (filter == "UK" ? "GB" : filter);
+    //     // return (
+    //     //     console.log(getFlags())
+    // //     // );
+    // }
 
     const handleClickDriverDetails = (driverId) => {
         console.log("driver id", driverId);
@@ -34,6 +46,7 @@ export default function Drivers() {
                     return (
                         <tr key={driver.Driver.driverId}>
                             <td>{driver.position} </td>
+                            {/* <td>{getFlags(driver.Driver.nationality)}</td> */}
                             <td onClick={() => handleClickDriverDetails(driver.Driver.driverId)}>
                                 {driver.Driver.givenName + " " + driver.Driver.familyName} </td>
                             <td>{driver.Constructors[0].name} </td>
