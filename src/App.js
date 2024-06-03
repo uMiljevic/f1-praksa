95fb87d52ffd6069e734ab4c3dcf9019c05090e2
uMiljevic <uros.milje@gmail.com>
2024-06-03 15:51:29 +0200

    Change Home Component and Utils Colors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link, Outlet } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Components/Home";
 import Drivers from "./Components/Drivers";
 import DriverDetails from "./Components/DriverDetails";
@@ -8,13 +8,10 @@ import Races from "./Components/Races";
 import Teams from "./Components/Teams";
 import React, { useEffect, useState } from "react";
 import Logo from "./images/f1 logo.png";
-import { Input, Space } from 'antd';
 import axios from "axios";
 
-function App() {
 
-  const { Search } = Input;
-  const onSearch = (value, _e, info) => console.log(info?.source, value);
+function App() {
   const [flags, setFlags] = useState([]);
 
   useEffect(() => {
@@ -31,29 +28,33 @@ function App() {
   return (
     <div className="main-container">
       <Router>
-        <nav className="nav-bar">
+        <div className="nav-position">
+          <nav className="nav-bar">
 
-          <div className="nav-img">
-            <Link to="/">
-              <img src={Logo} alt="F1 Logo" />
-            </Link>
-          </div>
+            <div className="nav-img">
+              <Link to="/">
+                <img src={Logo} alt="F1 Logo" />
+              </Link>
+            </div>
 
-          <div className="list-container">
-            <ul>
-              <li>
-                <Link to="/drivers">Drivers</Link>
-              </li>
-              <li>
-                <Link to="/teams" >Teams</Link>
-              </li>
-              <li>
-                <Link to="/races">Races</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-              
+            <div className="list-container">
+              <ul>
+                <li>
+                  <Link to="/drivers">Drivers</Link>
+                </li>
+                <li>
+                  <Link to="/teams" >Teams</Link>
+                </li>
+                <li>
+                  <Link to="/races">Races</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+
+        
+
         <Routes>
           <Route path="/" element={<Home flags={flags} />} />
           <Route path="/drivers" element={<Drivers flags={flags} />} />
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,47 @@
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import { YoutubeFilled, XOutlined, InstagramOutlined, TikTokOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
-
-export default function Home(){
-    return(
+export default function Home() {
+    const [text] = useTypewriter({
+        words: ['Formula 1', 'F1 Feeder', 'World Class Racing'],
+        loop: {},
+        typeSpeed: 100,
+        deleteSpeed: 120,
+    });
+    
+    
+    return (
         <div className="main-home-container">
 
-            
-            
-            <h1>Welcome to the F1 Feeder</h1>
-            
 
+            <div className="home-position">
+                <h1>
+                    Welcome To <span style={{fontWeight: 'bold', color: '#f44336'}}>
+                                    {text}
+                                </span>
+                    <span style={{color: 'red'}}>
+                        <Cursor />
+                    </span>
+                </h1>
+            </div>
+
+            <div className="footer-position">
+                <div className="footer">
+                    <Link to="https://www.youtube.com/@Formula1" target="_blanc">
+                    <YoutubeFilled className="icon"/>
+                    </Link>
+                    <XOutlined  className="icon"/>
+                    <InstagramOutlined  className="icon"/>
+                    <TikTokOutlined  className="icon"/>
+                </div>
+            </div>
+
+            <div className="overlay"></div>
+
+        <div className="video-container">
+          <video src={`${process.env.PUBLIC_URL}/assets/img/f1VideoBG.mp4`} autoPlay loop muted/>
+        </div>
         </div>
     );
 }
